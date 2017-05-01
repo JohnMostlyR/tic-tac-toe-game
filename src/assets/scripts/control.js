3334565254165e97171843466439bd2c0143d6cb
Johan Meester <walter.doodah@gmail.com>
2017-05-02 00:09:46 +0200

    Reformatting

--- src/assets/scripts/control.js
+++ src/assets/scripts/control.js
@@ -8,16 +8,16 @@ var globals = {};
  * when a level is clicked, it becomes highlighted and the "ai.level" variable
  * is set to the chosen level
  */
-$(".level").each(function() {
-    var $this = $(this);
-    $this.click(function() {
-        $('.selected').toggleClass('not-selected');
-        $('.selected').toggleClass('selected');
-        $this.toggleClass('not-selected');
-        $this.toggleClass('selected');
+$(".level").each(function () {
+  var $this = $(this);
+  $this.click(function () {
+    $('.selected').toggleClass('not-selected');
+    $('.selected').toggleClass('selected');
+    $this.toggleClass('not-selected');
+    $this.toggleClass('selected');
 
-        ai.level = $this.attr("id");
-    });
+    ai.level = $this.attr("id");
+  });
 });
 
 /*
@@ -25,16 +25,16 @@ $(".level").each(function() {
  * when start is clicked and a level is chosen, the game status changes to "running"
  * and UI view to swicthed to indicate that it's human's trun to play
  */
-$(".start").click(function() {
-    var selectedDiffeculty = $('.selected').attr("id");
-    if(typeof selectedDiffeculty !== "undefined") {
-        var aiPlayer = new AI(selectedDiffeculty);
-        globals.game = new Game(aiPlayer);
+$(".start").click(function () {
+  var selectedDiffeculty = $('.selected').attr("id");
+  if (typeof selectedDiffeculty !== "undefined") {
+    var aiPlayer = new AI(selectedDiffeculty);
+    globals.game = new Game(aiPlayer);
 
-        aiPlayer.plays(globals.game);
+    aiPlayer.plays(globals.game);
 
-        globals.game.start();
-    }
+    globals.game.start();
+  }
 });
 
 /*
@@ -43,21 +43,21 @@ $(".start").click(function() {
  * get the indecies of the clickd cell, craete the next game state, upadet the UI, and
  * advance the game to the new created state
  */
- $(".cell").each(function() {
-     var $this = $(this);
-     $this.click(function() {
-         if(globals.game.status === "running" && globals.game.currentState.turn === "X" && !$this.hasClass('occupied')) {
-             var indx = parseInt($this.data("indx"));
+$(".cell").each(function () {
+  var $this = $(this);
+  $this.click(function () {
+    if (globals.game.status === "running" && globals.game.currentState.turn === "X" && !$this.hasClass('occupied')) {
+      var indx = parseInt($this.data("indx"));
 
-             var next = new State(globals.game.currentState);
-             next.board[indx] = "X";
+      var next = new State(globals.game.currentState);
+      next.board[indx] = "X";
 
-             ui.insertAt(indx, "X");
+      ui.insertAt(indx, "X");
 
-             next.advanceTurn();
+      next.advanceTurn();
 
-             globals.game.advanceTo(next);
+      globals.game.advanceTo(next);
 
-         }
-     })
- });
+    }
+  })
+});
--- src/assets/scripts/ui.js
+++ src/assets/scripts/ui.js
@@ -15,54 +15,54 @@ ui.currentView = "";
 /*
  * starts the flickering effect of the robot image
  */
-ui.startRobotFlickering = function() {
-    ui.robotFlickeringHandle = setInterval(function() {
-        $("#robot").toggleClass('robot');
-    }, 500);
+ui.startRobotFlickering = function () {
+  ui.robotFlickeringHandle = setInterval(function () {
+    $("#robot").toggleClass('robot');
+  }, 500);
 };
 
 /*
  * stops the flickering effect on the robot image
  */
-ui.stopRobotFlickering = function() {
-    clearInterval(ui.robotFlickeringHandle);
+ui.stopRobotFlickering = function () {
+  clearInterval(ui.robotFlickeringHandle);
 };
 
 /*
  * switchs the view on the UI depending on who's turn it switchs
  * @param turn [String]: the player to switch the view to
  */
-ui.switchViewTo = function(turn) {
+ui.switchViewTo = function (turn) {
 
-    //helper function for async calling
-    function _switch(_turn) {
-        ui.currentView = "#" + _turn;
-        $(ui.currentView).fadeIn("fast");
+  //helper function for async calling
+  function _switch(_turn) {
+    ui.currentView = "#" + _turn;
+    $(ui.currentView).fadeIn("fast");
 
-        if(_turn === "ai")
-            ui.startRobotFlickering();
-    }
+    if (_turn === "ai")
+      ui.startRobotFlickering();
+  }
 
-    if(ui.intialControlsVisible) {
-        //if the game is just starting
-        ui.intialControlsVisible = false;
+  if (ui.intialControlsVisible) {
+    //if the game is just starting
+    ui.intialControlsVisible = false;
 
-        $('.intial').fadeOut({
-            duration : "slow",
-            done : function() {
-                _switch(turn);
-            }
-        });
-    }
-    else {
-        //if the game is in an intermediate state
-        $(ui.currentView).fadeOut({
-            duration: "fast",
-            done: function() {
-                _switch(turn);
-            }
-        });
-    }
+    $('.intial').fadeOut({
+      duration: "slow",
+      done: function () {
+        _switch(turn);
+      }
+    });
+  }
+  else {
+    //if the game is in an intermediate state
+    $(ui.currentView).fadeOut({
+      duration: "fast",
+      done: function () {
+        _switch(turn);
+      }
+    });
+  }
 };
 
 /*
@@ -71,15 +71,15 @@ ui.switchViewTo = function(turn) {
  * @param j [Number] : column number (0-indexed)
  * @param symbol [String]: X or O
  */
-ui.insertAt = function(indx, symbol) {
-    var board = $('.cell');
-    var targetCell = $(board[indx]);
+ui.insertAt = function (indx, symbol) {
+  var board = $('.cell');
+  var targetCell = $(board[indx]);
 
-    if(!targetCell.hasClass('occupied')) {
-        targetCell.html(symbol);
-        targetCell.css({
-            color : symbol == "X" ? "green" : "red"
-        });
-        targetCell.addClass('occupied');
-    }
+  if (!targetCell.hasClass('occupied')) {
+    targetCell.html(symbol);
+    targetCell.css({
+      color: symbol == "X" ? "green" : "red"
+    });
+    targetCell.addClass('occupied');
+  }
 }
